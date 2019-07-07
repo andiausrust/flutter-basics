86fa0b6df667e3e5bdec23ebe10f32af59b6d366
Andi <familie.mayer@aon.at>
2019-07-07 08:28:03 +0200

    setup

---
+++ lib/answer.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final Function _answerQuestion;
+  final String _answerText;
+  Answer(this._answerQuestion, this._answerText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: RaisedButton(
+        color: Colors.blueAccent,
+        textColor: Colors.white,
+        child: Text(_answerText),
+        onPressed: _answerQuestion,
+      ),
+    );
+  }
+}
